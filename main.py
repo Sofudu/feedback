d43e31483faf40ca787233fbb75d1cea93a51ad0
Sofudu <Sofudu@yandex.ru>
2021-04-21 22:56:44 +0300

    Ответная страница

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'i always say him that i like chocolate'
 
 
-@app.route("/")
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
+@app.route('/', methods=['GET', 'POST'])
+def register():
     form = FeedbackForm()
     if form.validate_on_submit():
-        # Эти три переменные на отправку на почту
         name = form.name.data
         email = form.email.data
         mess = form.mess.data
+        print(name, email, mess)
+        return redirect("/thank")
     return render_template('register.html', title='Обратная связь', form=form)
+
+
+@app.route("/thank")
+def thank():
+    return render_template("thank_you.html")
 
 
 if __name__ == '__main__':
